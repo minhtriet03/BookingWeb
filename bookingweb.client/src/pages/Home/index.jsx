4f05cd752dd910073f499ef321474fc31aa470f0
truonghiep <truonghiep051103@gmail.com>
2024-10-09 22:36:18 +0700

    Update Home v2

--- bookingweb.client/src/pages/Home/index.jsx
+++ bookingweb.client/src/pages/Home/index.jsx
@@ -1,14 +1,20 @@
-import Header from '../../component/Header';
-import Footer from '../../component/Footer';
-import SearchForm from './section1/index.jsx';
+import Header from '@/component/Header';
+import Footer from '@/component/Footer';
+import SearchForm from './SearchForm/index.jsx';
 import { Container } from 'react-bootstrap';
+import PromoSection from './PromoSection/index.jsx';
 
 function Home() {
   return (
     <>
       <Header />
-      <Container md>
+      <Container  style={{
+        maxWidth: '1100px',  // Hoặc kích thước bạn muốn
+        width: '100%',        // Chiếm toàn bộ chiều rộng
+        padding: '15px',      // Padding tùy chỉnh
+      }}>
         <SearchForm />
+        <PromoSection />
       </Container>
       <Footer />
     </>
