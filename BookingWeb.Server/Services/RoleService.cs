dc7db0afb10a7c06df313a1954acfe46b3dcef2e
truonghiep <truonghiep051103@gmail.com>
2024-11-11 21:22:35 +0700

    11/11

--- BookingWeb.Server/Services/RoleService.cs
+++ BookingWeb.Server/Services/RoleService.cs
@@ -1,6 +1,20 @@
-﻿namespace BookingWeb.Server.Services
+﻿using BookingWeb.Server.Interfaces;
+using BookingWeb.Server.Models;
+
+namespace BookingWeb.Server.Services
 {
     public class RoleService
     {
+
+        private readonly BookingBusContext _context;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IRoleRepository _roleRepository;
+
+        public RoleService(BookingBusContext context, IUnitOfWork unitOfWork, IRoleRepository roleRepository)
+        {
+            _context = context;
+            _roleRepository = roleRepository;
+            _unitOfWork = unitOfWork;
+        }
     }
 }
