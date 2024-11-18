fb505a9927e12b24e642c3d448a342e9f9ed6dac
triet3 <trietpt09@gmail.com>
2024-11-18 15:13:56 +0700

    11/18/2024

--- BookingWeb.Server/Services/LoaiXeService.cs
+++ BookingWeb.Server/Services/LoaiXeService.cs
@@ -6,37 +6,38 @@ using System.Threading.Tasks;
 namespace BookingWeb.Server.Services
 {
     public class LoaiXeService
-    {
-        private readonly ILoaiXeRepository _loaiXeRepository;
+    {   
+        
+        private readonly IUnitOfWork _unitOfWork;
 
-        public LoaiXeService(ILoaiXeRepository loaiXeRepository)
+        public LoaiXeService(IUnitOfWork unitOfWork)
         {
-            _loaiXeRepository = loaiXeRepository;
+             _unitOfWork = unitOfWork;
         }
 
         // Sử dụng async để gọi phương thức bất đồng bộ trong repository
         public async Task<List<Loaixe>> GetAllLoaiXes()
         {
-            return await _loaiXeRepository.GetAllAsync();
+            return await _unitOfWork.loaiXeRepository.GetAllAsync();
         }
 
         // Sử dụng async để gọi phương thức bất đồng bộ trong repository
         public async Task<Loaixe> GetLoaixe(int id)
         {
-            return await _loaiXeRepository.GetByIdAsync(id);
+            return await _unitOfWork.loaiXeRepository.GetByIdAsync(id);
         }
         public async Task<bool> AddLoaixe(Loaixe loaixe)
         {
-            return await _loaiXeRepository.AddAsync(loaixe);
+            return await _unitOfWork.loaiXeRepository.AddAsync(loaixe);
         }
         public async Task<bool> UpdateLoaixe(Loaixe loaixe)
         {
-            return await _loaiXeRepository.UpdateAsync(loaixe);
+            return await _unitOfWork.loaiXeRepository.UpdateAsync(loaixe);
         }
 
         public async Task<bool> DeleteLoaixe(int id)
         {
-            return await _loaiXeRepository.DeleteAsync(id);
+            return await _unitOfWork.loaiXeRepository.DeleteAsync(id);
         }
     }
 }
--- BookingWeb.Server/Services/XeService.cs
+++ BookingWeb.Server/Services/XeService.cs
@@ -7,36 +7,36 @@ namespace BookingWeb.Server.Services
 {
     public class XeService 
     {
-        private readonly IXeRepository _xerepository;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public XeService (IXeRepository repository)
+        public XeService (IUnitOfWork unitOfWork)
         {
-            _xerepository = repository;
+            _unitOfWork = unitOfWork;
         }
 
         // Sử dụng async để gọi phương thức bất đồng bộ trong repository
         public async Task<List<Xe>> GetAllXes()
         {
-            return await _xerepository.GetAllAsync();
+            return await _unitOfWork.xeRepository.GetAllAsync();
         }
 
         // Sử dụng async để gọi phương thức bất đồng bộ trong repository
         public async Task<Xe> Getxe(int id)
         {
-            return await _xerepository.GetByIdAsync(id);
+            return await _unitOfWork.xeRepository.GetByIdAsync(id);
         }
         public async Task<bool> Addxe(Xe xe)
         {
-            return await _xerepository.AddAsync(xe);
+            return await _unitOfWork.xeRepository.AddAsync(xe);
         }
         public async Task<bool> Updatexe(Xe xe)
         {
-            return await _xerepository.UpdateAsync(xe);
+            return await _unitOfWork.xeRepository.UpdateAsync(xe);
         }
 
         public async Task<bool> Deletexe(int id)
         {
-            return await _xerepository.DeleteAsync(id);
+            return await _unitOfWork.xeRepository.DeleteAsync(id);
         }
     }
 }
