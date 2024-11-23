992d8280c3de0c192a08d4429883c0790122dcd3
Toan-LD <101153912+Toan-LD@users.noreply.github.com>
2024-11-23 22:22:48 +0700

    sửa lại models thêm view admin

---
+++ BookingWeb.Server/Models/XeViTri.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookingWeb.Server.Models;
+
+public partial class XeViTri
+{
+    public int Id { get; set; }
+
+    public int? IdXe { get; set; }
+
+    public int? IdViTri { get; set; }
+
+    public virtual Vitri? IdViTriNavigation { get; set; }
+
+    public virtual Xe? IdXeNavigation { get; set; }
+}
