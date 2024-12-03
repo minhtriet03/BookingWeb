3eb927f6ee117f25da93b53dcc72ea2134b8fb5c
trunghieu <3121410197@sv.sgu.edu.vn>
2024-12-03 22:41:06 +0700

    +

--- bookingweb.client/src/pages/Booking/DatGhe/index.jsx
+++ bookingweb.client/src/pages/Booking/DatGhe/index.jsx
@@ -1,5 +1,4 @@
-﻿import { Container, Row, Col, Alert, Form, Button } from 'react-bootstrap';
-import { useRef } from 'react';
+﻿import { Container, Row, Col, Alert, Button } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 import './datghe.css';
 import RenderSeats from './renderSeats';
@@ -12,62 +11,22 @@ function DatGhe({ handleDisplay, infoTuyenDuong }) {
         { color: "#DEF3FF", label: "Còn trống" }, // Ghế trống
         { color: "#FDEDE8", label: "Đang chọn" }, // Ghế đang chọn
     ];
-
-    const handleBack = () => {
-        handleDisplay();
-    }
-    const [formData, setFormData] = useState({
-        fullName: "",
-        phoneNumber: "",
-        email: "",
-    });
-
-    const [errors, setErrors] = useState({});
-    const formRef = useRef(null); // Tham chiếu đến biểu mẫu để gọi các phương thức từ bên ngoài
-
-    // Hàm kiểm tra hợp lệ
-    const validate = () => {
-        const newErrors = {};
-        if (!formData.fullName.trim()) {
-            newErrors.fullName = "Họ và tên là bắt buộc.";
-        }
-        const phoneRegex = /^[0-9]{10}$/;
-        if (!formData.phoneNumber) {
-            newErrors.phoneNumber = "Số điện thoại là bắt buộc.";
-        } else if (!phoneRegex.test(formData.phoneNumber)) {
-            newErrors.phoneNumber = "Số điện thoại không hợp lệ (10 số).";
-        }
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!formData.email) {
-            newErrors.email = "Email là bắt buộc.";
-        } else if (!emailRegex.test(formData.email)) {
-            newErrors.email = "Email không hợp lệ.";
-        }
-
-        setErrors(newErrors);
-        return Object.keys(newErrors).length === 0;
-    };
-
-    // Xử lý khi nhấn nút trong div khác
-    const handleExternalSubmit = (e) => {
-        if (validate()) {
-            e.preventDefault();
-            alert("Biểu mẫu hợp lệ!");
-            navigate("/thanh-toan");
-        }
-    };
-
-    // Xử lý khi nhập dữ liệu
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-        setErrors({ ...errors, [name]: "" });
-    };
-
     const [selectedSeats, setSelectedSeats] = useState({});
     const [bookedSeats, setBookedSeats] = useState({});
     const [selectedSeatCount, setSelectedSeatCount] = useState(0);
     const [showLimitAlert, setShowLimitAlert] = useState(false);
+
+    const handleBack = () => {
+        handleDisplay();
+    }
+    // Xử lý khi nhấn nút trong div khác
+    const handleExternalSubmit = (e) => {
+        //if (validate()) {
+            e.preventDefault();
+            alert("Biểu mẫu hợp lệ!");
+            navigate("/thanh-toan");
+        //}
+    };
 
     useEffect(() => {
         const fetchedBookedSeats = ["A01", "B03", "A06"];
@@ -129,57 +88,36 @@ function DatGhe({ handleDisplay, infoTuyenDuong }) {
                                 ))}
                             </Row>
                         </Row>
-                        <Row className="mt-2 w-100 border m-auto" />
+                        {/*<Row className="mt-2 w-100 border m-auto" />*/}
                     </Col>
                     <Col>
                         <Row className="border bg-white rounded-4 p-3 mb-1">
                             <h5 className="my-3 ">Thông tin khách hàng</h5>
-                                <Form autoComplete="off" ref={formRef}>
-                                <Form.Group className="mb-3" controlId="formBasicFullName">
-                                    <Form.Label className="formLabel mb-1">Họ và tên</Form.Label>
-                                        <Form.Control
-                                            type="text"
-                                            placeholder="Họ và tên"
-                                            name="fullName"
-                                            value={formData.fullName}
-                                            onChange={handleChange}
-                                            isInvalid={!!errors.fullName}
-                                        />
-                                        <Form.Control.Feedback type="invalid">
-                                            {errors.fullName}
-                                        </Form.Control.Feedback>
-                                    </Form.Group>
-
-                                    <Form.Group className="mb-3" controlId="formBasicPhone">
-                                    <Form.Label className="formLabel mb-1">Số điện thoại</Form.Label>
-                                        <Form.Control
-                                            type="text"
-                                            placeholder="Số điện thoại"
-                                            name="phoneNumber"
-                                            value={formData.phoneNumber}
-                                            onChange={handleChange}
-                                            isInvalid={!!errors.phoneNumber}
-                                        />
-                                        <Form.Control.Feedback type="invalid">
-                                            {errors.phoneNumber}
-                                        </Form.Control.Feedback>
-                                    </Form.Group>
-
-                                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                                    <Form.Label className="formLabel mb-1">Email</Form.Label>
-                                        <Form.Control
-                                            type="email"
-                                            placeholder="Email"
-                                            name="email"
-                                            value={formData.email}
-                                            onChange={handleChange}
-                                            isInvalid={!!errors.email}
-                                        />
-                                        <Form.Control.Feedback type="invalid">
-                                            {errors.email}
-                                        </Form.Control.Feedback>
-                                    </Form.Group>
-                                </Form>
+                            <Row className="d-flex justify-content-between">
+                                <Col xs={4} className="text-gray">
+                                    Họ và tên:
+                                </Col>
+                                <Col xs={8} className="text-end text-black">
+                                    Phạm Trung Hiếu
+                                    {/*{Noi_KhoiHanh || ''} - {Noi_Den || ''}*/}
+                                </Col>
+                            </Row>
+                            <Row className="mt-2 d-flex justify-content-between">
+                                <Col xs={5} className="text-gray">
+                                    Số điện thoại:
+                                </Col>
+                                <Col xs={7} className="text-end text-black">
+                                    0337518038 {/*{Noi_KhoiHanh || ''} - {Noi_Den || ''}*/}
+                                </Col>
+                            </Row>
+                            <Row className="mt-2 d-flex justify-content-between">
+                                <Col xs={5} className="text-gray">
+                                    Email
+                                </Col>
+                                <Col xs={7} className="text-end text-black">
+                                    ailahieu123@gmail.com {/*{Noi_KhoiHanh || ''} - {Noi_Den || ''}*/}
+                                </Col>
+                            </Row>
                         </Row>
                         <Row className="border bg-white rounded-4 p-3 mb-3">
                             <h5 className="my-3">Thông tin lượt đi</h5>
@@ -215,7 +153,7 @@ function DatGhe({ handleDisplay, infoTuyenDuong }) {
                                 <Col xs={7} className="text-end text-black">
                                     {selectedSeatCount > 0
                                         ? selectedSeatIds.slice().reverse().join(", ") // Đảo ngược mảng và nối chuỗi
-                                        : <div></div>}
+                                        : <div>--</div>}
                                 </Col>
                             </Row>
 
@@ -228,6 +166,7 @@ function DatGhe({ handleDisplay, infoTuyenDuong }) {
                             </Row>
                         </Row>
                         <Row className="border bg-white rounded-4 p-3 mb-1">
+                            <h5 className="my-3 ">Thông tin giá</h5>
                             <div className="mt-2 d-flex justify-content-between">
                                 <h5 className="text-end text-black p-2">
                                     190.000đ
