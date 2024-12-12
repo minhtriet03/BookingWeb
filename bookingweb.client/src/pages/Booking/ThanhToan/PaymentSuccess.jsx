64891952e49bc1e6dc9571d9ab186f501c2c8954
truonghiep <truonghiep051103@gmail.com>
2024-12-12 11:44:00 +0700

    done

--- bookingweb.client/src/pages/Booking/ThanhToan/PaymentSuccess.jsx
+++ bookingweb.client/src/pages/Booking/ThanhToan/PaymentSuccess.jsx
@@ -1,16 +1,50 @@
 ﻿import { useLocation } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { axiosInstance } from '@/utils/axiosInstance';
+
 
 function PaymentSuccess() {
+
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     const responseCode = queryParams.get("vnp_ResponseCode");
+    const orderId = queryParams.get("idPhieuDat");
+    const id_chuyenxe = useSelector((state) => state.chuyenxe.idcx);
+    const vexe = useSelector((state) => state.vexe.vexeOrder);
+
+
     const message =
         responseCode === "00"
             ? "Thanh toán thành công! Cảm ơn bạn đã sử dụng dịch vụ."
             : "Thanh toán thất bại. Vui lòng thử lại.";
+
+    useEffect(() => {
+        const updatePaymentStatus = async () => {
+            if (responseCode === "00") {
+                try {
+                    const response = await axiosInstance.post('/api/thanh-toan/update-status', {
+                        IdPhieuDat: orderId,
+                        idcx: id_chuyenxe,
+                        vexe: vexe
+                    });
+
+                    if (response.data.success) {
+                        console.log("Cập nhật trạng thái thành công.");
+                    } else {
+                        console.error("Cập nhật trạng thái thất bại.");
+                    }
+                } catch (error) {
+                    console.error("Lỗi khi gọi API cập nhật trạng thái:", error);
+                }
+            }
+        };
+
+        updatePaymentStatus();
+    }, [responseCode, orderId]);
 
     return (
         <div className="container my-5">
